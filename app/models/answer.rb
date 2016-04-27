f0dec8a22bb0aef39dbf87d55da7d6696fd22334
VladimirMozgin <mozgin.vova@gmail.com>
2016-04-27 09:09:30 +0300

    add validation in answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
 
-  validates :body, presence: true
+  validates :body, :question, presence: true
   validates :body, length: { in: 1..10000 }
 end
