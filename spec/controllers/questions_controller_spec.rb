35ed44223192dc838a8fd96528ff16d43936ce9b
VladimirMozgin <mozgin.vova@gmail.com>
2016-04-28 16:17:35 +0300

    add answer new and create

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #show' do
     before { get :show, id: question }
 
-    it 'assigns the request question to questions' do
+    it 'assigns the request question to @question' do
       expect(assigns(:question)).to eq question
     end
 
@@ -43,7 +43,7 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #edit' do
     before { get :edit, id: question }
 
-    it 'assigns the request question to questions' do
+    it 'assigns the request question to @question' do
       expect(assigns(:question)).to eq question
     end
 
