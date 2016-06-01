27b6e4cdd23d032edbd9cf1bff7e2c17433a3096
VladimirMozgin <mozgin.vova@gmail.com>
2016-06-01 16:55:32 +0300

    add test for edit answer

--- spec/acceptance/answers/edit_answer_spec.rb
+++ spec/acceptance/answers/edit_answer_spec.rb
@@ -9,6 +9,7 @@ feature 'Answer editing', %q{
   given(:user)      { create(:user) }
   given!(:question) { create(:question, user: user) }
   given!(:answer)   { create(:answer, question: question, user: user) }
+  given(:new_user) { create(:user) }
 
   scenario 'Unauthenticated user try to edit question' do
     visit question_path(question)
@@ -16,30 +17,35 @@ feature 'Answer editing', %q{
   end
 
   describe 'Authenticated user' do
-    before do
-      sign_in user
-      visit question_path(question)
-    end
-
-    scenario 'sees link to Edit' do
-      within '.answers' do
-        expect(page).to have_link 'Edit'
+    context 'for himself' do
+      before do
+        sign_in user
+        visit question_path(question)
       end
-    end
 
-    scenario 'try to edit his answer', js: true do
-      within '.answers' do
-        click_on 'Edit'
-        fill_in 'Answer', with: 'edited answer'
-        click_on 'Save'
+      scenario 'sees link to Edit' do
+        within '.answers' do
+          expect(page).to have_link 'Edit'
+        end
+      end
 
-        expect(page).to have_content answer.body
-        expect(page).to have_content 'edited answer'
-        expect(page).to_not have_selector 'textarea'
+      scenario 'try to edit his answer', js: true do
+        within '.answers' do
+          click_on 'Edit'
+          fill_in 'Answer', with: 'edited answer'
+          click_on 'Save'
+
+          expect(page).to have_content answer.body
+          expect(page).to have_content 'edited answer'
+          expect(page).to_not have_selector 'textarea'
+        end
       end
     end
 
     scenario "try to edit other user's question" do
+      sign_in new_user
+      visit question_path(question)
+      expect(page).to_not have_link 'Edit'
     end
   end
 end
