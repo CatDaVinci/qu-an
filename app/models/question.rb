963cba8ce9c43cb3fdd04949ef24aee2530e0267
VladimirMozgin <mozgin.vova@gmail.com>
2016-06-06 23:36:27 +0300

    Question accepted nested attributes for attachments

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,6 +7,8 @@ class Question < ActiveRecord::Base
   validates :body, length: { in: 1..10000 }
   validates :title, length: { in: 1..100 }
 
+  accepts_nested_attributes_for :attachments
+
   def order_answers_by_best
     answers.order("best desc")
   end
