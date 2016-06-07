1602c5767a43d77e24ad2ac5b5c548e815e98bbc
VladimirMozgin <mozgin.vova@gmail.com>
2016-06-07 08:48:30 +0300

    refactoring

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -1,10 +1,16 @@
 # encoding: utf-8
 
 class FileUploader < CarrierWave::Uploader::Base
+  delegate :identifier, to: :file
+
   storage :file
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  #method delegator / proxy to other method
+  # def indentifier
+  #   self.file.identifier
+  # end
 end
